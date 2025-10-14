716c784c8951f3cb3b313f284310077b46361a82
Deyvison Samuel Gomes do Nascimento <deyvisonnascimento2025@gmail.com>
2025-10-14 20:19:06 -0300

    fix: correções na ordem de leitura dos arquivos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Orquestra tudo: importa dados e funções, calcula e exibe
-import { produtos, carrinho, TAXA_DESCONTO } from "./dados.js"
+import { produtos, carrinho, TAXA_DESCONTO,  } from "./dados.js"
 import { totalCarrinho, aplicarDesconto, formatarBRL, valorDoFrete } from "./funcoes.js"
 
 // 1) Calcula total bruto
@@ -16,18 +16,19 @@ console.log(
 )
 console.log("Total final:", formatarBRL(totalComDesconto))
 
-// 4) (Opcional) Atualiza a página, se existir HTML com esses IDs
+// 4) Calcular o valor do frete
+const frete =  valorDoFrete(totalComDesconto) 
+
+// 5) (Opcional) Atualiza a página, se existir HTML com esses IDs
 const elBruto = document.getElementById("total-bruto")
-const elFinal = document.getElementById("total-final")
 const elDesconto = document.getElementById("total-desconto")
 const elFrete = document.getElementById("total-frete")
+const elFinal = document.getElementById("total-final")
 
 if (elBruto && elFinal && elDesconto && elFrete) {
   elBruto.textContent = formatarBRL(totalBruto)
-  elFinal.textContent = formatarBRL(totalComDesconto)
   elDesconto.textContent = formatarBRL(totalBruto - totalComDesconto)
-  elFrete.textContent = formatarBRL(totalComDesconto)
+  elFrete.textContent = formatarBRL(frete)
+  elFinal.textContent = formatarBRL(totalComDesconto + frete)
 }
 
-// 4) Calcular o valor do frete
-const frete =  valorDoFrete(totalComDesconto)
